c3a5062a9316546232418bf9ce480e735aba53dc
sakayat <sakhyatkabir1@gmail.com>
2024-06-10 17:44:11 +0600

    bank_account, address model added

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from .constants import account_type, gender_type
 # Create your models here.
+
+class UserBankAccount(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="account")
+    account_type = models.CharField(max_length=10, choices=account_type)
+    account_no = models.IntegerField(unique=True)
+    birth_date = models.DateTimeField(blank=True, null=True)
+    gender = models.CharField(choices=gender_type)
+    initial_deposit_date = models.DateField(auto_now_add=True)
+    balance = models.DecimalField(default=0, max_digits=14, decimal_places=2)
+    
+
+class UserAddress(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="address")
+    street_address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    postal_code = models.IntegerField()
+    country = models.CharField(max_length=100)
+    
