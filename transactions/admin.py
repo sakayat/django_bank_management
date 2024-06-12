baee0b93f12e89e518f1aa00b35bc1d57059e32a
sakayat <sakhyatkabir1@gmail.com>
2024-06-13 01:11:35 +0600

    added custom loan admin

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,5 +1,16 @@
+from typing import Any
 from django.contrib import admin
 from .models import TransactionsModel
 
 # Register your models here.
-admin.site.register(TransactionsModel)
+@admin.register(TransactionsModel)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ["account", "amount", "balance_after_transactions", "transaction_type", "loan_approve"]
+    
+    def save_model(self, request, obj, form, change):
+        
+        if obj.loan_approve == True:
+            obj.account.balance += obj.amount
+            obj.balance_after_transactions = obj.account.balance
+            obj.account.save()
+        return super().save_model(request, obj, form, change)
