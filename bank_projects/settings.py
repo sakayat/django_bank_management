0dbad43fd23246638d9de88891eb3f58a1e6f38c
sakayat <sakhyatkabir1@gmail.com>
2024-06-14 14:57:03 +0600

    updated

--- bank_projects/settings.py
+++ bank_projects/settings.py
@@ -68,15 +68,22 @@ WSGI_APPLICATION = 'bank_projects.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': os.getenv("DB_NAME"),
+#        'USER': os.getenv("DB_USER"),
+#        'PASSWORD': os.getenv("DB_USER_PASSWORD"),
+#        'HOST': os.getenv("DB_HOST"),
+#        'PORT': os.getenv("DB_PORT"),
+#    }
+# }
+
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': os.getenv("DB_NAME"),
-       'USER': os.getenv("DB_USER"),
-       'PASSWORD': os.getenv("DB_USER_PASSWORD"),
-       'HOST': os.getenv("DB_HOST"),
-       'PORT': os.getenv("DB_PORT"),
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
