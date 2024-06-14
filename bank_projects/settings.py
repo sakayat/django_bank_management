199dfbccf07021ec2480728c783486ea8f74fbfe
sakayat <sakhyatkabir1@gmail.com>
2024-06-14 16:33:49 +0600

    added database

--- bank_projects/settings.py
+++ bank_projects/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 load_dotenv()
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -80,10 +81,9 @@ WSGI_APPLICATION = 'bank_projects.wsgi.application'
 # }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+     'default': dj_database_url.config(
+         default=os.getenv('DATABASE_URL')
+     )
 }
 
 # Password validation
